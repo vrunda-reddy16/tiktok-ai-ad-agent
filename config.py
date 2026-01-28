9d51f809ba1a40d7f2414d4e9e2851b144cca0d3
vrunda-reddy16 <vrundareddy1664@gmail.com>
2026-01-28 18:01:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# config.py
+
+ALLOWED_OBJECTIVES = ["Traffic", "Conversions"]
+MAX_AD_TEXT_LENGTH = 100
+
+# OAuth simulation config
+DEFAULT_TOKEN_STATUS = "valid"  # valid, expired, invalid, geo_blocked
 
